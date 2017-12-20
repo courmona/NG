afbad6d35cf8cf7af0c47e46d0305e2acfa9d79a
Axel Courmont <34718068+courmona@users.noreply.github.com>
2017-12-20 20:45:14 +0100

    Update console.java

--- Programmes/src/console.java
+++ Programmes/src/console.java
@@ -2,10 +2,11 @@
 public class console {
 
 	public static void main(String[] args) {
-		System.out.println("Ligne de code du programme � voir dans git");
+		System.out.println("Ligne de code du programme à voir dans git");
 		for (int i=0;i<10;i++) {
 			System.out.println("i= "+i);
 		}
+		System.out.println("modif à partir de github.com").
 
 	}
 
