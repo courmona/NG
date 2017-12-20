51dd7edc4e1ea64b1d8d0ea35c8a5dd7cf8f95a5
Axel <Axel@Axel.home>
2017-12-20 20:39:45 +0100

    cond for

--- Programmes/src/console.java
+++ Programmes/src/console.java
@@ -3,6 +3,9 @@ public class console {
 
 	public static void main(String[] args) {
 		System.out.println("Ligne de code du programme à voir dans git");
+		for (int i=0;i<10;i++) {
+			System.out.println("i= "+i);
+		}
 
 	}
 
